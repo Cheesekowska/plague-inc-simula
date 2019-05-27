993ec4d2bae89b3268c969d76d6920a4f5177246
PickedaUserNejm <50627253+PickedaUserNejm@users.noreply.github.com>
2019-05-27 21:38:42 +0200

    Update WayOfInfection.java

--- PlagueIncSimulation/src/WayOfInfection.java
+++ PlagueIncSimulation/src/WayOfInfection.java
@@ -1,11 +1,18 @@
+import java.util.Random;
+
 public class WayOfInfection {
 	
-	private String name;
-	private double rateOfInfection;
+	
+	public double[] tableOfFactors;
 
-	WayOfInfection(String name,double rateOfInfection)
+	WayOfInfection()
 	{
-		this.name=name;
-		this.rateOfInfection=rateOfInfection;
+		
+		tableOfFactors=new double[3];
+		Random r=new Random();
+		this.tableOfFactors[0]=r.nextDouble()+1;
+		this.tableOfFactors[1]=r.nextDouble()+1;
+		this.tableOfFactors[2]=r.nextDouble()+1;
 	}
 }
+
