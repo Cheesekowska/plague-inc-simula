9998dad4beafabf6f1233d9f48d7ad92bb4f9ead
PickedaUserNejm <50627253+PickedaUserNejm@users.noreply.github.com>
2019-05-27 21:38:21 +0200

    Update Symptom.java

--- PlagueIncSimulation/src/Symptom.java
+++ PlagueIncSimulation/src/Symptom.java
@@ -1,11 +1,17 @@
+import java.util.Random;
+
 public class Symptom {
 	
-	private String name;
-	private double rateOfInfection;
+	public double []tableOfFactors;
 
-	Symptom(String name,double rateOfInfection)
+
+	Symptom()
 	{
-		this.name=name;
-		this.rateOfInfection=rateOfInfection;
+		
+		this.tableOfFactors= new double[3];
+		Random r=new Random();
+		this.tableOfFactors[0]=r.nextDouble()+1;
+		this.tableOfFactors[1]=r.nextDouble()+1;
+		this.tableOfFactors[2]=r.nextDouble()+1;
 	}
 }
