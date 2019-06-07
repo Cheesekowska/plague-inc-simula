2cfe755fe32b89ac99f4bad7d4b6a34a618d0682
PickedaUserNejm <50627253+PickedaUserNejm@users.noreply.github.com>
2019-06-07 20:59:49 +0200

    Update Symptom.java

--- PlagueIncSimulation/src/Symptom.java
+++ PlagueIncSimulation/src/Symptom.java
@@ -1,10 +1,21 @@
 import java.util.Random;
 
 public class Symptom {
+	
+	/**
+	 * This class is used to change the way <i> Disease</i> kills people.
+	 * It contains factors, which have an influence on how many people 
+	 * are killed by it.
+	 */
 
 	public double[] tableOfFactors;
 
 	Symptom() {
+		
+		/**
+		 * This method is a simple constructor. It gives random values
+		 * to all three factors.
+		 */
 
 		this.tableOfFactors = new double[3];
 		Random r = new Random();
