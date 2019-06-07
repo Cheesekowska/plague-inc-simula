0f57ebdf743b6b2ae20f2b13fd91609f16c66915
PickedaUserNejm <50627253+PickedaUserNejm@users.noreply.github.com>
2019-06-07 20:53:31 +0200

    Update WayOfInfection.java

--- PlagueIncSimulation/src/WayOfInfection.java
+++ PlagueIncSimulation/src/WayOfInfection.java
@@ -2,11 +2,21 @@ import java.util.Random;
 
 public class WayOfInfection {
 	
+	/**
+	 * This class is used to change the way <i> Disease</i> behaves.
+	 * It contains factors, which have an influence on how many people 
+	 * are infected by the <i>Disease</i>
+	 */
 	
 	public double[] tableOfFactors;
 
 	WayOfInfection()
 	{
+	
+		/**
+		 * This method is a simple constructor. It gives random values
+		 * to all three factors.
+		 */
 		
 		tableOfFactors=new double[3];
 		Random r=new Random();
